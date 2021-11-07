a54a63b06d893b53c1c29951a740bd6663295ee3
Chrisjan69 <83349786+Chrisjan69@users.noreply.github.com>
2021-11-07 14:16:43 +0200

    connected to DB, Populated DB with Faker, Displayed fake Data as test.

--- CMPG_Proj2/image_app/views.py
+++ CMPG_Proj2/image_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from image_app.models import Topic,Webpage,AccessRecord
 # Create your views here.
 
 def index(request):
-    my_dict =  {'insert_me':'Hello, now i am coming from image_app/index.html!!'}
-    return render(request,'image_app/index.html', context = my_dict)
+
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'acces_records': webpages_list}
+
+    return render(request,'image_app/index.html', context = date_dict)
 
 def test(request):
     my_dict =  {'insert_me':'Hello, now i am coming from image_app/index.html!!'}
